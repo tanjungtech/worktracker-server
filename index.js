46f411a17663551a753dc68ef4fa42e04aa7072f
Farid <tanjung.technology@gmail.com>
2023-02-19 17:09:19 +0700

    Initialize server

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(process.env.SERVER_PORT, () => console.log(`Example app listening on port ${process.env.SERVER_PORT}!`))
